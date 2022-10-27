b7a889cb096ebab21f0c46dda919a722fc1c4a17
Vere_ia36 <Vere_ia36@student.itstep.org>
2022-10-27 15:02:48 +0300

    Add UI methods

---
+++ ClientApp/Entities/CommandParam.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ClientApp.Entities
+{
+    public class CommandParam<Parameter> : ICommand
+    {
+        readonly Action<Parameter> _execute;
+        readonly Predicate<Parameter> _canExecute;
+
+        /// <summary>
+        /// Creates a new command that can always execute.
+        /// </summary>
+        /// <param name="execute">The method to execute</param>
+        public CommandParam(Action<Parameter> execute) : this(execute, null) { }
+
+        /// <summary>
+        /// Creates a new command.
+        /// </summary>
+        /// <param name="execute">The method to execute</param>
+        /// <param name="canExecute">can execute or not</param>
+        public CommandParam(Action<Parameter> execute, Predicate<Parameter> canExecute)
+        {
+            if (execute == null)
+                return;
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (parameter is Parameter p)
+                _execute?.Invoke(p);
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (parameter is Parameter p)
+                return _canExecute == null ? true : _canExecute(p);
+            return false;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
---
+++ ClientApp/NewAccountWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static data_access_library.PasswordManagerDbContext;
+
+namespace ClientApp
+{
+    /// <summary>
+    /// Interaction logic for NewAccountWindow.xaml
+    /// </summary>
+    public partial class NewAccountWindow : Window
+    {
+        public User AccountModel = new User();
+        public Action AddAccountCallback { get; set; }
+        public NewAccountWindow()
+        {
+            InitializeComponent();
+            DataContext = AccountModel;
+        }
+
+        public void ResetAccountContext()
+        {
+            AccountModel = new User();
+            this.DataContext = AccountModel;
+        }
+
+        private void AddAccountCallbackFunc() { AddAccountCallback?.Invoke(); }
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Escape: this.Hide(); break;
+                case Key.Enter: AddAccountCallbackFunc(); this.Hide(); break;
+            }
+        }
+
+        private void PasteTextToBox(object sender, RoutedEventArgs e)
+        {
+            switch (int.Parse(((Button)e.Source).Uid))
+            {
+                case 1: a.Text = Clipboard.GetText(); break;
+                case 2: b.Text = Clipboard.GetText(); break;
+            }
+        }
+
+        private void ClearTextClick(object sender, RoutedEventArgs e)
+        {
+            switch (int.Parse(((Button)e.Source).Uid))
+            {
+                case 1: a.Text = ""; break;
+                case 2: b.Text = ""; break;
+            }
+        }
+
+        private void AddAccountClick(object sender, RoutedEventArgs e)
+        {
+            AddAccountCallbackFunc();
+            this.Hide();
+        }
+    }
+}
+
