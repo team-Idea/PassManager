300cf0172a8e2a2b4a0aaff079ad1578183c2bc9
Vere_ia36 <Vere_ia36@student.itstep.org>
2022-10-31 11:44:36 +0200

    Check Add null pass

--- ClientApp/PasswordManagerModel.cs
+++ ClientApp/PasswordManagerModel.cs
@@ -1,5 +1,6 @@
 ﻿using data_access_library;
 using data_access_library.Repositories;
+
 using static data_access_library.PasswordManagerDbContext;
 
 namespace ClientApp
@@ -7,17 +8,17 @@ namespace ClientApp
     internal class PasswordManagerModel
     {
         public PasswordManagerDbContext context;
-        IRepository<User> userRep;
-        IRepository<Login_Item> loginRep;
-        IRepository<Credit_Card> creditcardRep;
-        IRepository<Personal_Info> personalinfoRep;
+        //IRepository<User> userRep;
+       // IRepository<Login_Item> loginRep;
+       // IRepository<Credit_Card> creditcardRep;
+       // IRepository<Personal_Info> personalinfoRep;
         public PasswordManagerModel()
         {
             context = new PasswordManagerDbContext();
-            userRep = new Repository<User>(context);
-            loginRep = new Repository<Login_Item>(context);
-            creditcardRep = new Repository<Credit_Card>(context);
-            personalinfoRep = new Repository<Personal_Info>(context);
+          //  userRep = new Repository<User>(context);
+          //  loginRep = new Repository<Login_Item>(context);
+          //  creditcardRep = new Repository<Credit_Card>(context);
+         //   personalinfoRep = new Repository<Personal_Info>(context);
         }
     }
 }
