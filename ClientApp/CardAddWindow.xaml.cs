d1e9c0e36903e085a45f82e78aaa911f673cacdc
Sasha Jakob <sashayakub8@gmail.com>
2022-10-31 20:03:27 +0200

    added ui layout

--- ClientApp/CardAddWindow.xaml.cs
+++ ClientApp/CardAddWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using data_access_library;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,22 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ClientApp
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class CardAddWindow : Window
     {
-        
-        public MainWindow()
+        public CardAddWindow()
         {
-           
             InitializeComponent();
-            PasswordManagerDbContext dbContext = new PasswordManagerDbContext();
         }
     }
 }
--- ClientApp/InfoAddWindow.xaml.cs
+++ ClientApp/InfoAddWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using data_access_library;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,22 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ClientApp
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for InfoAddWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class InfoAddWindow : Window
     {
-        
-        public MainWindow()
+        public InfoAddWindow()
         {
-           
             InitializeComponent();
-            PasswordManagerDbContext dbContext = new PasswordManagerDbContext();
         }
     }
 }
