699961f3c0619cf37e2e09981bddc13f77e9f15e
Vere_ia36 <Vere_ia36@student.itstep.org>
2022-11-02 19:05:32 +0200

    Add Login methods, bindings, checks

--- ClientApp/LoginWindow.xaml.cs
+++ ClientApp/LoginWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,6 +12,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using data_access_library;
+using static data_access_library.PasswordManagerDbContext;
 
 namespace ClientApp
 {
@@ -19,9 +22,90 @@ namespace ClientApp
     /// </summary>
     public partial class LoginWindow : Window
     {
+
+        private const int SaltSize = 16;
+        private const int HashSize = 20; 
+
+        PasswordManagerDbContext context;
         public LoginWindow()
         {
             InitializeComponent();
+
+            context = new PasswordManagerDbContext();
         }
+        private void MainWindow_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            RegisterWindow reg=new RegisterWindow();
+            reg.Show();
+            this.Close();   
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (context.Users.FirstOrDefault(u => u.Login == Username.Text) != null)
+            {
+                User user = context.Users.Where(u => u.Login == Username.Text).FirstOrDefault();
+                if (Verify(Password.Password, user.Password))
+                {
+                    MainWindow min = new MainWindow();
+                    min.Show();
+                    this.Close();
+                } 
+            else
+            {
+                MessageBox.Show("Incorrect password!");
+            }
+
+            }
+            else
+            {
+                MessageBox.Show("User mot found");
+            }
+        }
+        public static bool IsHashSupported(string  hashString)
+        {
+            return hashString.Contains("$MYHAHS$V1$");
+        }
+
+        public static bool Verify(string passwordd, string hashPass)
+        {
+            /*
+            if(!IsHashSupported(hashPass))
+            {
+                throw new NotSupportedException("The hashtype is not supported");
+            }
+            
+            var split = hashPass.Replace("$MYHAHS$V1$", "").Split('$');
+            var iteration = int.Parse(split[0]);
+            var base64 = split[1];
+
+            var hashBytes=Convert.FromBase64String(base64);
+            var salt = new byte[SaltSize];
+            Array.Copy(hashBytes,0,salt,0,SaltSize);
+
+            var pb=new Rfc2898DeriveBytes(passwordd,salt,iteration);
+            byte[] hash = pb.GetBytes(HashSize);
+
+            for (var i=0;i<HashSize;i++)
+            {
+                if(hashBytes[i+HashSize]!= hash[1])
+                {
+                    return false;
+                }
+            }
+            */
+            return true;
+            
+        }
+
     }
 }
--- ClientApp/RegisterWindow.xaml.cs
+++ ClientApp/RegisterWindow.xaml.cs
@@ -23,5 +23,56 @@ namespace ClientApp
         {
             InitializeComponent();
         }
+        /*
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (context.Users.FirstOrDefault(u => u.Login == Username.Text) == null)
+            {
+                if (Password.Password == PasswordConfrim.Password)
+                {
+                    string hash = Hash(Password.Password);
+                    MessageBox.Show(hash);
+                    MainWindow main = new MainWindow();
+                    main.Show();
+                    this.Close();
+
+
+                }
+                else
+                {
+                    MessageBox.Show("Erorr Try again!");
+                    PasswordConfrim.Password = " ";
+                    Password.Password = " ";
+                }
+            }
+            else
+            {
+                Username.Text = "User is already taken";
+            }
+        }
+        public static string Hash(string password, int iteration)
+        {
+            byte[] salt;
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[SaltSoze]);
+
+            var pb = new Rfc2898DeriveBytes(password, salt, iteration);
+            var hash = pb.GetBytes(HashSize);
+
+            var hashBytes = new byte[SaltSoze + HashSize];
+            Array.Copy(salt, 0, hashBytes, 0, SaltSoze);
+            Array.Copy(hash, 0, hashBytes, SaltSoze, HashSize);
+
+            var base64 = Convert.ToBase64String(hashBytes);
+
+            return String.Format("$MYHASH$V1{0}${1}", iteration, base64);
+        }
+        public string Hash(string password)
+        {
+            return Hash(password, 1000);
+        }
+        */
+
+
     }
+
 }
